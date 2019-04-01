1184a54148bc3d8018bcd3bd4ae990c2ce5cde69
Leonor Madureira <NB20985@novabase.pt>
2019-04-01 18:16:56 +0100

    Initial commit

---
+++ src/main/java/com/celfocus/integratedtestsuserservice/IntegratedTestsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.celfocus.integratedtestsuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedTestsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratedTestsUserServiceApplication.class, args);
+	}
+
+}
